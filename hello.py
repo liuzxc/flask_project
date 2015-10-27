e88f0e74878bfc6282fe302832979cb97e8d9f35
liuzxc <lxq_102172@163.com>
2015-10-28 00:29:17 +0800

    add ignore file

--- hello.py
+++ hello.py
@@ -14,6 +14,14 @@ def index():
 def user(name):
   return render_template('user.html', name=name)
 
+@app.errorhandle(404)
+def page_not_found(e):
+  return render_template('404.html', 404)
+
+@app.errorhandle(500)
+def internal_server_error(e):
+  return render_template('500.html', 500)
+
 if __name__ == '__main__':
   # app.run(debug=True)
   manager.run()
