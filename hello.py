247b9d7eca7eb55ccd18307e31a84cf04ce6e984
liuzxc <lxq_102172@163.com>
2015-10-27 23:10:23 +0800

    my first python flask app

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+  return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+  # app.run(debug=True)
+  manager.run()
