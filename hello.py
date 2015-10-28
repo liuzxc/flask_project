2e1209a76ec254cc1e70bad3a6c911eccc6a4c3a
liuzxc <lxq_102172@163.com>
2015-10-28 13:34:01 +0800

    使用模版

--- hello.py
+++ hello.py
@@ -1,26 +1,30 @@
 from flask import Flask, render_template
+from datetime import datetime
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html',
+                          current_time = datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
   return render_template('user.html', name=name)
 
-@app.errorhandle(404)
+@app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html', 404)
+  return render_template('404.html'), 404
 
-@app.errorhandle(500)
+@app.errorhandler(500)
 def internal_server_error(e):
-  return render_template('500.html', 500)
+  return render_template('500.html'), 500
 
 if __name__ == '__main__':
   # app.run(debug=True)
